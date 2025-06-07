4a22112deb743a8481dbba4105067e1427e2fd3c
Chris Petta <cpetta@pm.me>
2025-06-07 15:01:41 -0400

    foundation for dynamically loaded sub pages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
-console.log('hello world');
+"use-strict"
+console.log('loaded js')
+// ---------------------------------
+// Selectors
+// ---------------------------------
+
+// ---------------------------------
+// Events
+// ---------------------------------
+window.addEventListener('load', init_view_welcome_screen);
+
+
+// ---------------------------------
+// Functions
+// ---------------------------------
+async function init_view_welcome_screen() {
+	load_view("welcome");
+}
+
+async function load_view(view_name) {
+	try {
+		const response = await fetch(`${view_name}.html`);
+		
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`);
+		}
+
+		const json = await response.text();
+		console.log(json);
+	}
+	catch (error) {
+		console.error(error.message);
+	}
+}
