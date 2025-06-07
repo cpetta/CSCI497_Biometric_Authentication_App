97a8f26269be6adfdb6fb83af475d7cb2e71cf98
Chris Petta <cpetta@pm.me>
2025-06-07 16:21:00 -0400

    add welcome view

---
+++ js/welcome.js
@@ -0,0 +1,27 @@
+import { load_view } from './page_loader.js'
+
+console.log('welcome.js')
+// ---------------------------------
+// Selectors
+// ---------------------------------
+const base = document.getElementById('welcome-view');
+const login_btn = base.querySelector('.welcome-login-btn');
+const create_account_btn = base.querySelector('.welcome-create-account-btn');
+
+// ---------------------------------
+// Events
+// ---------------------------------
+login_btn.addEventListener('click', handle_login_btn_click);
+create_account_btn.addEventListener('click', handle_create_account_btn_click)
+// ---------------------------------
+// Functions
+// ---------------------------------
+
+
+function handle_login_btn_click() {
+	load_view("login");
+}
+
+function handle_create_account_btn_click() {
+	load_view("create_account");
+}
