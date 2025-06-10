705655283718d53fcac0264fa18076b1c5c4f2fd
Chris Petta <cpetta@pm.me>
2025-06-09 20:24:10 -0400

    add account management page

---
+++ js/account_management.js
@@ -0,0 +1,20 @@
+"use-strict"
+import { load_view } from './page_loader.js'
+//import { load_view } from './js/page_loader.js'
+import { init as init_camera_controls } from './camera_control.js'
+
+// ---------------------------------
+// Selectors
+// ---------------------------------
+const base = document.getElementById('manage-account-view');
+const back_btn = base.querySelector('.back-btn');
+
+// ---------------------------------
+// init
+// ---------------------------------
+init_camera_controls();
+
+// ---------------------------------
+// Events
+// ---------------------------------
+back_btn.addEventListener('click', () => load_view('welcome'));
--- js/welcome.js
+++ js/welcome.js
@@ -10,11 +10,15 @@ const base = document.getElementById('welcome-view');
 const login_btn = base.querySelector('.welcome-login-btn');
 const create_account_btn = base.querySelector('.welcome-create-account-btn');
 
+// Temp
+base.querySelector('.welcome-manage-account-btn').addEventListener('click', () => load_view("account_management"));
+
 // ---------------------------------
 // Events
 // ---------------------------------
 login_btn.addEventListener('click', handle_login_btn_click);
 create_account_btn.addEventListener('click', handle_create_account_btn_click)
+
 // ---------------------------------
 // Functions
 // ---------------------------------
