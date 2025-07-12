2aa17c643ef187edeb367b9a635dc42345a7f2db
Chris Petta <cpetta@pm.me>
2025-07-12 08:04:33 -0400

    Added build and start script

--- build_serverEXE.bat
+++ build_serverEXE.bat
@@ -1,2 +1,8 @@
-pyinstaller -D --add-data assest:assets --add-data css:css --add-data js:js --add-data views:views .\server.py
-Read-Host -Prompt "Press Enter to exit"
+del /s /q .\build
+rmdir /s /q .\build
+
+del /s /q .\dist
+rmdir /s /q .\dist
+
+pyinstaller -D .\server.py --add-data assets:assets --add-data css:css --add-data js:js --add-data views:views
+pause
